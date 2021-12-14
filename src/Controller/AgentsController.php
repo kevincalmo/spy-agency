ad1e926eaf89a6ff4ee3bd9c9b5d1f098aa44b91
kevincalmo <kevin.calmo@gmx.fr>
2021-12-14 18:53:05 -0500

    Création du controlleur agents

---
+++ src/Controller/AgentsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\Agents;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AgentsController extends AbstractController
+{
+    /**
+     * @Route("/agents", name="agents")
+     */
+    public function index(): Response
+    {
+        
+        $agent = (new Agents())
+            ->setLastName('Doe')
+            ->setFirstName('John')
+            ->setAuthentificationCode('y9FFmmJ35')
+            ->setNationality('France')
+            ->setPassword('ih46K7MDc');
+    
+        return $this->render('agents/index.html.twig', [
+            'agent' => $agent,
+        ]);
+    }
+}
