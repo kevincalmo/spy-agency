992e0eaeea9d83f6f7013c7745c82f53455ca875
kevincalmo <kevin.calmo@gmx.fr>
2021-12-23 13:49:46 -0500

    création du formulaire des spécialités

---
+++ src/Form/SpecialityType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Speciality;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SpecialityType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            //->add('agents')
+            //->add('missions')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Speciality::class,
+        ]);
+    }
+}
