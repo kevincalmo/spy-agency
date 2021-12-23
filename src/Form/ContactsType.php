3fa3aae6c194705791a15ac082dc94bee47c82c1
kevincalmo <kevin.calmo@gmx.fr>
2021-12-23 10:53:57 -0500

    Création du formulaire de contact

---
+++ src/Form/ContactsType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contacts;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('last_name')
+            ->add('first_name')
+            ->add('birth_date')
+            ->add('code_name')
+            ->add('nationality', ChoiceType::class, [
+                'choices' => [
+                    'France'=> 'french',
+                    'Espagne'=>'spanish'
+                ],
+            ])
+            //->add('mission')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contacts::class,
+        ]);
+    }
+}
