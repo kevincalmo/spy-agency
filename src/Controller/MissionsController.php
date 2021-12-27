3338e593baf9957b73bd6cc3959b436b63eea64a
kevincalmo <kevin.calmo@gmx.fr>
2021-12-27 15:16:07 -0500

    Gestion de la validation des données de mission

--- src/Controller/MissionsController.php
+++ src/Controller/MissionsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Missions;
 use App\Form\MissionsType;
+use App\Repository\AgentsRepository;
 use App\Repository\MissionsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,58 +25,133 @@ class MissionsController extends AbstractController
         $missions = $missionsRepository->findAll();
 
         return $this->render('missions/index.html.twig', [
-            'items'=>$missions,
-            'type'=>'missions'
+            'items' => $missions,
+            'type' => 'missions'
         ]);
     }
 
-     /**
+    /**
      * @Route("/add-mission" , name="add-mission")
      */
-    public function addMission(Request $request): Response
+    public function addMission(Request $request, AgentsRepository $agentsRepository): Response
     {
         $mission = new Missions;
-        $form = $this->createForm(MissionsType::class,$mission);
+        $form = $this->createForm(MissionsType::class, $mission);
         $form->handleRequest($request);
+        $errorsForm = [];
 
-            if($form->isSubmitted() && $form->isValid())
-            {
-                $entityManager = $this->getdoctrine()->getManager();
-                $entityManager->persist($mission);
-                $entityManager->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            //Vérification des spécificités de mission
+
+
+            /* dd($mission->getAgents()); */
+            /**
+             * initialisation du tableau d'eerues du formulaire
+             * @var array
+             */
+            
+
+
+            $countryMissionForm = $mission->getCountry();
+            $specialityMissionForm = $mission->getSpecialitys();
+
+
+
+            $countryAgentsForm = [];
+            foreach ($mission->getAgents() as $agent) {
+                $countryAgentsForm[] = $agent->getNationality();
             }
 
-            
-        
-        
+            $specialityAgentsForm = [];
+
+            $countryContactsForm = [];
+            foreach ($mission->getContacts() as $contact) {
+                
+                if($contact->getNationality() !== $countryMissionForm){
+                    $errorsForm[]= "Les contacts présents sur la mission doivent avoir la nationalité du lieu de la mission";
+                }
+            }
+
+            $countryTargetsForm = [];
+            foreach ($mission->getTargets() as $target) {
+                $countryTargetsForm[] = $target->getNationality();
+            }
+
+            $countryStashsForm = [];
+            foreach ($mission->getStashs() as $stash) {
+                if($stash->getCountry() !== $countryMissionForm){
+                    $errorsForm[]= "Les planques présents sur la mission doivent avoir la nationalité du lieu de la mission";
+                }
+            }
+
+            /*================ Vérification au niveau des agents ================*/
+            $i=0;
+            $n = 1;
+            foreach ($mission->getAgents() as $agent) {
+
+                
+                $agent = $agentsRepository->find($agent->getId());
+                foreach($agent->getSpecialitys() as $speciality){
+                    
+                    foreach($specialityMissionForm as $missionSpeciality){
+                        if($speciality === $missionSpeciality) $i++;
+                    }
+                }
+
+                
+
+                foreach($mission->getTargets() as $target){
+                    if($agent->getNationality() === $target->getNationality()){
+                        $errorsForm[]= "Les agents et les cibles ne peuvent avoir les même nationalités";
+                    }
+                }
+
+
+            }
+
+            if($i === 0) $errorsForm[]= "Aucun agent sélectionné ne possède la spécialité requise pour cette mission";
+
+            dump($specialityMissionForm);
+
+            if (empty($errorsForm)) {
+                /* $entityManager = $this->getdoctrine()->getManager();
+                                $entityManager->persist($mission);
+                                $entityManager->flush(); */
+                
+            }
+        }
+
+
+
+
         return $this->render('form-item.html.twig', [
-           'form'=>$form->createView(),
-           'type'=>'mission',
-           'function'=>'Creer'
+            'form' => $form->createView(),
+            'type' => 'mission',
+            'function' => 'Creer',
+            'errorsValidation'=>$errorsForm,
         ]);
     }
 
     /**
      * @Route("/edit-mission/{id}" , name="edit-mission")
      */
-    public function editAgent($id ,Request $request, MissionsRepository $missionsRepository): Response
+    public function editAgent($id, Request $request, MissionsRepository $missionsRepository): Response
     {
         $mission = $missionsRepository->find($id);
-        $form = $this->createForm(MissionsType::class,$mission);
+        $form = $this->createForm(MissionsType::class, $mission);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid())
-        {
+        if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getdoctrine()->getManager();
             $entityManager->flush();
             return $this->redirectToRoute("missions");
         }
 
-            
+
         return $this->render('form-item.html.twig', [
-           'form'=>$form->createView(),
-           'type'=>'mission',
-           'function'=>'Editer'
+            'form' => $form->createView(),
+            'type' => 'mission',
+            'function' => 'Editer'
         ]);
     }
 }
